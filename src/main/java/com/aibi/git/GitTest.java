f29dede8d56d223fc201aeb0151f7c2d1fc13e06
Aibi <735060236@qq.com>
2022-06-24 10:55:19 +0800

    second commit

--- src/main/java/com/aibi/git/GitTest.java
+++ src/main/java/com/aibi/git/GitTest.java
@@ -3,5 +3,6 @@ package com.aibi.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello2 git");
     }
 }
