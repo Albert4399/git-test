64518a0736f436a4948236c967334d792f0c39f1
Aibi <735060236@qq.com>
2022-06-24 11:02:06 +0800

    hit-fix commit

--- src/main/java/com/aibi/git/GitTest.java
+++ src/main/java/com/aibi/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello2 git");
         System.out.println("hello3 git");
+        System.out.println("hello4 git");
     }
 }
