04fa27fd98e05d321f84549b90980dd0fb64be0f
Aibi <735060236@qq.com>
2022-06-24 10:56:12 +0800

    third commit

--- src/main/java/com/aibi/git/GitTest.java
+++ src/main/java/com/aibi/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello2 git");
+        System.out.println("hello3 git");
     }
 }
